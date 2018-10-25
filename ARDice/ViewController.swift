b1be488d9846aa598681268f1650f2b1d73c1121
Li Fangchen <thristram@gmail.com>
2018-10-25 22:33:45 +0800

    Initial Commit

---
+++ ARDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARDice
+//
+//  Created by Fangchen Li on 10/25/18.
+//  Copyright © 2018 Fangchen Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
